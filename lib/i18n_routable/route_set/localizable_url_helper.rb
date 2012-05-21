d256118d4233eeb1e7424f02d1170a39fc06babd
Erik Ogan and Thomas Shafer <tech_ops+erik+thomas@change.org>
2012-05-21 11:37:29 -0700

    better test coverage for the TranslationAssistant which splits up routes into segments and later translates those segment

--- lib/i18n_routable/route_set/localizable_url_helper.rb
+++ lib/i18n_routable/route_set/localizable_url_helper.rb
@@ -2,43 +2,43 @@ module I18nRoutable
   module RouteSet
     module LocalizableUrlHelper
 
-    def define_hash_access_with_localize(route, name, kind, options)
-      selector = hash_access_name(name, kind)
+      def define_hash_access_with_localize(route, name, kind, options)
+        selector = hash_access_name(name, kind)
 
-      # Rails.application.routes.named_routes[:edit_petition]
-      i18n_segments = route.segment_keys.find_all{|k| k.to_s =~ /^i18n/}.inject({}){|hash, segment|
-        hash.update segment => segment.to_s.match(/^i18n_(.*$)/)[1]
-      }
+        # Rails.application.routes.named_routes[:edit_petition]
+        i18n_segments = route.segment_keys.find_all{|k| k.to_s =~ /^i18n/}.inject({}){|hash, segment|
+          hash.update segment => segment.to_s.match(/^i18n_(.*$)/)[1]
+        }
 
-      # We use module_eval to avoid leaks
-      @module.module_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def #{selector}(options = {})                                      # def hash_for_users_url(options = nil)
-          route = _routes.named_routes[#{options[:use_route].inspect}]
+        # We use module_eval to avoid leaks
+        @module.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def #{selector}(options = {})                                      # def hash_for_users_url(options = nil)
+            route = _routes.named_routes[#{options[:use_route].inspect}]
 
-          base_options = #{options.inspect}
+            base_options = #{options.inspect}
 
-          options[:locale] ||= I18n.locale
+            options[:locale] ||= I18n.locale
 
-          #{i18n_segments.inspect}.each_pair do |option, segment|
-            base_options[option] ||= begin
-              I18nRoutable.translate_segment(segment, options[:locale])
+            #{i18n_segments.inspect}.each_pair do |i18n_segment, segment|
+              base_options[i18n_segment] ||= begin
+                I18nRoutable.translate_segment(segment, options[:locale])
+              end
             end
-          end
 
-          if options[:locale] == I18n.default_locale
-            options.delete(:locale)
-          else
-            options[:locale] = I18nRoutable.convert_to_display_locale(options[:locale])
-          end
+            if options[:locale] == I18n.default_locale
+              options.delete(:locale)
+            else
+              options[:locale] = I18nRoutable.convert_to_display_locale(options[:locale])
+            end
 
-          options.reverse_merge!(base_options)                              #   options ? {:only_path=>false}.merge(options) : {:only_path=>false}
+            options.reverse_merge!(base_options)                              #   options ? {:only_path=>false}.merge(options) : {:only_path=>false}
 
-          options
-        end                                                                 # end
-        protected :#{selector}                                              # protected :hash_for_users_url
-      RUBY
-      helpers << selector
-    end
+            options
+          end                                                                 # end
+          protected :#{selector}                                              # protected :hash_for_users_url
+        RUBY
+        helpers << selector
+      end
 
       def self.included(base)
         base.alias_method_chain :define_hash_access, :localize
