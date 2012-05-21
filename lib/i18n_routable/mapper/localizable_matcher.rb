17efe69899b025a8ff54c1c3d49077fa7b7a3c1b
Erik Ogan and Thomas Shafer <tech_ops+erik+thomas@change.org>
2012-05-21 11:37:29 -0700

    better test coverage for the TranslationAssistant which splits up routes into segments and later translates those segment

--- lib/i18n_routable/mapper/localizable_matcher.rb
+++ lib/i18n_routable/mapper/localizable_matcher.rb
@@ -2,25 +2,24 @@ module I18nRoutable
   module Mapper
     module LocalizableMatcher
 
-    def match_with_localize path, options={}
-      options ||= {}
+      def match_with_localize path, options={}
+        options ||= {}
 
-      path, segments = I18nRoutable.convert_path_to_localized_regexp(path)
+        path, segments = I18nRoutable.convert_path_to_localized_regexp(path)
 
-      if segments.present?
-        options[:constraints] ||= {}
-        if options[:constraints].is_a? Hash
-          I18nRoutable.add_segment_constraints(options[:constraints], segments)
+        if segments.present?
+          options[:constraints] ||= {}
+          if options[:constraints].is_a? Hash
+            I18nRoutable.add_segment_constraints(options[:constraints], segments)
+          end
         end
+
+        match_without_localize(path, options)
       end
 
-      match_without_localize(path, options)
-    end
-
-    def self.included base
-      base.alias_method_chain :match, :localize
-    end
-
+      def self.included base
+        base.alias_method_chain :match, :localize
+      end
 
     end
   end
---
+++ spec/i18n_routable/translation_assistant_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+describe I18nRoutable::TranslationAssistant do
+
+  subject { I18nRoutable }
+
+  context '#convert_path_to_localized_regexp' do
+
+    def convert path
+      subject.send(:convert_path_to_localized_regexp, path)
+    end
+
+    it "should translate the components of a normal url" do
+      convert("/posts").should eql ["/:i18n_posts", ["posts"]]
+      convert("/posts/new").should eql ["/:i18n_posts/:i18n_new", ["posts", "new"]]
+    end
+
+    it "should ignore param names" do
+      convert("/posts/:posts").should eql ["/:i18n_posts/:posts", ["posts"]]
+      convert("/posts(/:id)(.:format)").should eql ["/:i18n_posts(/:id)(.:format)", ["posts"]]
+      convert('all-the-posts(/:action(/:id))').should eql [":i18n_all_the_posts(/:action(/:id))", ["all-the-posts"]]
+      convert(':alias/events/:old_action').should eql [":alias/:i18n_events/:old_action", ["events"]]
+    end
+
+    it "should preserve order" do
+      convert("/more-posts").should eql ["/:i18n_more_posts", ["more-posts"]]
+      convert("/posts/more-posts").should eql ["/:i18n_posts/:i18n_more_posts", ["posts", "more-posts"]]
+      convert("/more-posts/posts").should eql ["/:i18n_more_posts/:i18n_posts", ["more-posts", "posts"]]
+    end
+  end
+
+  context '#route_constraint_for_segment' do
+
+    def convert segment
+      subject.send :route_constraint_for_segment, segment
+    end
+
+    it 'should translate' do
+      convert("posts").should == /posts|puestos|messages/
+    end
+
+    it 'should regexp escape' do
+      convert("ur.l").should == /ur\.l/
+    end
+
+    it 'should cgi escape' do
+      convert("ur*l").should == /ur%2Al/
+    end
+  end
+
+end
