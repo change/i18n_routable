61d6c96e01e89ecfa70bac2413b390d41df7540e
Erik Ogan and Thomas Shafer <tech_ops+erik+thomas@change.org>
2012-05-21 11:56:35 -0700

    removed double include weirdness, I don't know why I did that in the first place

--- lib/i18n_routable/mapper.rb
+++ lib/i18n_routable/mapper.rb
@@ -1,13 +1,5 @@
 require 'i18n_routable/mapper/localizable_scope'
 require 'i18n_routable/mapper/localizable_matcher'
 
-module I18nRoutable
-  module Mapper
-
-    include I18nRoutable::Mapper::LocalizableScope
-
-  end
-end
-
-ActionDispatch::Routing::Mapper.send :include, I18nRoutable::Mapper
+ActionDispatch::Routing::Mapper.send :include, I18nRoutable::Mapper::LocalizableScope
 ActionDispatch::Routing::Mapper::Base.send :include, I18nRoutable::Mapper::LocalizableMatcher
--- lib/i18n_routable/route_set.rb
+++ lib/i18n_routable/route_set.rb
@@ -1,13 +1,5 @@
 require 'i18n_routable/route_set/reverse_route_lookup'
 require 'i18n_routable/route_set/localizable_url_helper'
 
-module I18nRoutable
-  module RouteSet
-
-    include I18nRoutable::RouteSet::ReverseRouteLookup
-
-  end
-end
-
-ActionDispatch::Routing::RouteSet.send :include, I18nRoutable::RouteSet
+ActionDispatch::Routing::RouteSet.send :include, I18nRoutable::RouteSet::ReverseRouteLookup
 ActionDispatch::Routing::RouteSet::NamedRouteCollection.send :include, I18nRoutable::RouteSet::LocalizableUrlHelper
